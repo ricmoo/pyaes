3f2bc626874b0186317c2e6e6106c738baf4eb93
adityag3 <adityagovil3@gmail.com>
2016-10-31 16:07:10 +0530

    demo code file added

--- implementation.py
+++ implementation.py
@@ -1,4 +1,5 @@
 import pyaes
+from time import time
 
 # A 256 bit (32 byte) key
 key = "123456789012345ef890123456789012"
@@ -10,23 +11,29 @@ iv = "9988776655443310"
 
 # Each block into the mode of operation must be a multiple of the segment
 # size. For this example we choose 8 bytes.
+s = time()
 aes = pyaes.AESModeOfOperationCFB(key, iv = iv, segment_size = 4)
-plaintext =  "1234567890123456"
+plaintext =  "baf9dffc8b690539"
 ciphertext = aes.encrypt(plaintext)
 
 # '''v\xa9\xc1w"\x8aL\x93\xcb\xdf\xa0/\xf8Y\x0b\x8d\x88i\xcb\x85rmp
 #    \x85\xfe\xafM\x0c)\xd5\xeb\xaf'''
 #print repr(ciphertext)
 
-
+p = time()
+m = time() - s
+print m
 # The cipher-block chaining mode of operation maintains state, so 
 # decryption requires a new instance be created
 aes = pyaes.AESModeOfOperationCFB(key, iv = iv, segment_size = 4)
 decrypted = aes.decrypt(ciphertext)
+
+print time() - p
 #print decrypted
 # True
 #print decrypted == plaintext
 
+print "CFB Mode"
 print "Unencrypted data: ", plaintext
 print "Encrypted data  : ", ciphertext
 print "Decrypted data  : ", decrypted
